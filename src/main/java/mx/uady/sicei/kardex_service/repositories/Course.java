8a38fcbc003190b79ef6301dfe13ac54ac462e42
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2025-05-12 22:04:55 -0600

    feat: :fire: remove unnecessary classes

--- src/main/java/mx/uady/sicei/kardex_service/repositories/Course.java
+++
@@ -1,9 +0,0 @@
-package mx.uady.sicei.kardex_service.repositories;
-
-import lombok.Data;
-
-@Data
-public class Course {
-    private String id;
-    private String name;
-}
--- src/main/java/mx/uady/sicei/kardex_service/repositories/Grade.java
+++
@@ -1,8 +0,0 @@
-package mx.uady.sicei.kardex_service.repositories;
-
-import lombok.Data;
-
-@Data
-public class Grade {
-    private Double value;
-}
