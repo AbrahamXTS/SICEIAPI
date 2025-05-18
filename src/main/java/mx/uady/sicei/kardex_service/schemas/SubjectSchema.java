d9c04c2e4e88b46132cfeca20119169275c5adf6
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2025-05-18 02:15:43 -0600

    style: :art: format code for consistency in CourseType, BaseSchema, SubjectSchema, and TeacherSchema

--- src/main/java/mx/uady/sicei/kardex_service/schemas/SubjectSchema.java
+++ src/main/java/mx/uady/sicei/kardex_service/schemas/SubjectSchema.java
@@ -1,13 +1,12 @@
 package mx.uady.sicei.kardex_service.schemas;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,13 +20,13 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @Entity(name = "subject")
 public class SubjectSchema extends BaseSchema {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
 
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String name;
 
-    @OneToMany(mappedBy = "subject")
-    private List<CourseSchema> courses;
+  @OneToMany(mappedBy = "subject")
+  private List<CourseSchema> courses;
 }
--- src/main/java/mx/uady/sicei/kardex_service/schemas/TeacherSchema.java
+++ src/main/java/mx/uady/sicei/kardex_service/schemas/TeacherSchema.java
@@ -1,13 +1,12 @@
 package mx.uady.sicei.kardex_service.schemas;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,22 +20,21 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @Entity(name = "teacher")
 public class TeacherSchema extends BaseSchema {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String employeeId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String employeeId;
 
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String name;
 
-    @Column(nullable = false)
-    private String lastName;
+  @Column(nullable = false)
+  private String lastName;
 
-    @Column
-    private String secondLastName;
+  @Column private String secondLastName;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @OneToMany(mappedBy = "teacher")
-    private List<CourseSchema> coursesTaught;
+  @OneToMany(mappedBy = "teacher")
+  private List<CourseSchema> coursesTaught;
 }
